b24ccd69e7e804810634b88369011e22953bd0cb
Mosab Ahmed Irwished <42383497+Mosab97@users.noreply.github.com>
2024-12-26 13:47:48 +0200

    context api

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -4,6 +4,7 @@ import { Outlet } from "react-router-dom";
 export default function DefaultLayout() {
   return (
     <div>
+      DefaultLayout
       <Outlet />
     </div>
   );
---
+++ react/src/contexts/ContextProvider.jsx
@@ -0,0 +1,34 @@
+import { Children, createContext, useContext, useState } from "react";
+
+const StateContext = createContext({
+  user: null,
+  token: null,
+  setUser: () => {},
+  setToken: () => {},
+});
+
+export const ContextProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const [token, _setToke] = useState(localStorage.getItem("ACCESS_TOKEN"));
+  const setToken = (token) => {
+    _setToke(token);
+    if (token) {
+      localStorage.setItem("ACCESS_TOKEN", token);
+    } else {
+      localStorage.removeItem("ACCESS_TOKEN");
+    }
+  };
+  return (
+    <StateContext.Provider
+      value={{
+        user,
+        token,
+        setUser,
+        setToken,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+export const useStateContext = () => useContext(StateContext);
